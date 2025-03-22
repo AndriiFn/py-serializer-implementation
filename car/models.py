5c5ca3d5df0e3ae9954ad48d2c252770bed9c873
Andrii <andriyfedchuk2006@gmail.com>
2025-03-22 09:35:31 +0100

    Solution2

--- car/models.py
+++ car/models.py
@@ -1,15 +1,23 @@
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import (
+    MaxValueValidator,
+    MinValueValidator)
 from django.db import models
 
 
 class Car(models.Model):
     manufacturer = models.CharField(max_length=64)
     model = models.CharField(max_length=64)
-    horse_powers = models.PositiveSmallIntegerField(
-        validators=[MaxValueValidator(1914), MinValueValidator(1)]
-    )
+    horse_powers = (
+        models.PositiveSmallIntegerField(
+        validators=
+        [MaxValueValidator(1914),
+         MinValueValidator(1)]
+    ))
     is_broken = models.BooleanField()
-    problem_description = models.TextField(null=True, blank=True)
+    problem_description = models.TextField(
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return f"{self.manufacturer}, {self.model}"
