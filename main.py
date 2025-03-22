f26beb13c2d017e0f86b5c865c79558285ad2668
Andrii <andriyfedchuk2006@gmail.com>
2025-03-22 09:32:57 +0100

    Solution

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    return CarSerializer(car).data
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    return CarSerializer(json).data
